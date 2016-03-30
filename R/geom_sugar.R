561c1d4e81452e87ef54cc6961b78fbb9bb3ce7b
Hiren Joshi <hirenj@gmail.com>
2016-03-30 10:33:47 +0200

    Add override for colour

--- R/geom_sugar.R
+++ R/geom_sugar.R
@@ -3,7 +3,7 @@
 #' @export
 geom_sugar <- function(mapping = NULL, data = NULL, stat = "identity",
                           position = "identity",
-                          show.legend = NA, inherit.aes = TRUE,na.rm=T,offset=0,...) {
+                          show.legend = NA, inherit.aes = TRUE,na.rm=T,offset=0,fill=NULL,...) {
   ggplot2::layer(
     data = data,
     mapping = mapping,
@@ -15,31 +15,42 @@ geom_sugar <- function(mapping = NULL, data = NULL, stat = "identity",
     params = list(
       na.rm=na.rm,
       offset=offset,
+      fill=fill,
       ...
     )
   )
 }
 
-draw_sugar = function(x,y,sugar,offset) {
+draw_sugar = function(x,y,sugar,offset,fill=NULL) {
+  args = list(x=x,y=y,offset=offset)
+  if (! is.na(fill)) {
+    args[['fill']] = fill
+  }
+  func = NULL
+
   if (sugar == "galnac") {
-    return (draw_galnac(x,y,offset))
+    func = draw_galnac
   }
   if (sugar == "man") {
-    return (draw_man(x,y,offset))
+    func = draw_man
   }
   if (sugar == "gal(b1-3)galnac") {
-    return (draw_galb13galnac(x,y,offset))
+    func = draw_galb13galnac
+  }
+
+  if ( ! is.null(func) ) {
+    return(do.call(func,args))
   }
 }
 
-draw_man = function(x,y,offset=0) {
+draw_man = function(x,y,offset=0,fill='green') {
   grid::circleGrob(
     x, grid::unit(y,"native") + grid::unit(offset * .pt,"mm"),
     r = grid::unit(0.5 * .pt ,"mm"),
     default.units = "native",
     gp = grid::gpar(
       col = ggplot2::alpha("black", 1),
-      fill = ggplot2::alpha("green", 1),
+      fill = ggplot2::alpha(fill, 1),
       lwd = 0.5 * .pt,
       lty = 1,
       lineend = "butt"
@@ -47,7 +58,7 @@ draw_man = function(x,y,offset=0) {
   )
 }
 
-draw_galnac = function(x,y,offset=0) {
+draw_galnac = function(x,y,offset=0,fill='yellow') {
   grid::rectGrob(
     x, grid::unit(y,"native") + grid::unit(offset * .pt,"mm"),
     width = grid::unit(1 * .pt ,"mm"),
@@ -56,7 +67,7 @@ draw_galnac = function(x,y,offset=0) {
     just = c("centre", "centre"),
     gp = grid::gpar(
       col = ggplot2::alpha("black", 1),
-      fill = ggplot2::alpha("yellow", 1),
+      fill = ggplot2::alpha(fill, 1),
       lwd = 0.5 * .pt,
       lty = 1,
       lineend = "butt"
@@ -64,15 +75,15 @@ draw_galnac = function(x,y,offset=0) {
   )
 }
 
-draw_galb13galnac = function(x,y,offset=0) {
-  galnac = draw_galnac(x,y,offset)
+draw_galb13galnac = function(x,y,offset=0,fill='yellow') {
+  galnac = draw_galnac(x,y,offset,fill)
   gal = grid::circleGrob(
     x, grid::unit(y,"native") + grid::unit((offset+1) * .pt,"mm"),
     r = grid::unit(0.5 * .pt ,"mm"),
     default.units = "native",
     gp = grid::gpar(
       col = ggplot2::alpha("black", 1),
-      fill = ggplot2::alpha("yellow", 1),
+      fill = ggplot2::alpha(fill, 1),
       lwd = 0.5 * .pt,
       lty = 1,
       lineend = "butt"
@@ -86,10 +97,10 @@ draw_galb13galnac = function(x,y,offset=0) {
 #' @export
 GeomSugar <- ggplot2::ggproto("GeomSugar", ggplot2::Geom,
                         required_aes=c('x','y','class'),
-                        draw_panel = function(data, panel_scales, coord,offset=0) {
+                        draw_panel = function(data, panel_scales, coord,offset=0,fill=NULL) {
                           coords <- coord$transform(data, panel_scales)
                           draw_sugar_vec = Vectorize(draw_sugar,SIMPLIFY=F)
-                          results = draw_sugar_vec(coords$x,coords$y,coords$class,offset)
+                          results = draw_sugar_vec(coords$x,coords$y,coords$class,offset,ifelse(is.null(fill),NA,fill))
                           do.call(grid::gList,results)
                         }
 )
